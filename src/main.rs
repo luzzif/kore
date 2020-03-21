fc5a9edcdf9d0c9369c8cc004a41766c91739786
luzzif <federico.luzzi@mondora.com>
2020-03-21 17:42:35 +0100

    chore: minor code cleaning

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,26 @@
 #![no_std]
 #![feature(panic_handler)]
 #![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
-
 #[macro_use]
 extern crate lazy_static;
 extern crate spin;
 extern crate volatile;
-
 mod vga;
-
+use core::panic::PanicInfo;
 use core::fmt::Write;
 use vga::writer::VGA_WRITER;
 
-#[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     write!(VGA_WRITER.lock(), "Hello world!");
     loop {}
 }
 
-use core::panic::PanicInfo;
 
 #[cfg(not(test))]
 #[panic_handler]
 #[no_mangle]
-pub fn panic(info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
     write!(VGA_WRITER.lock(), "{}", info).unwrap();
     loop {}
 }
